259629c27a64f42d1df2bd3a5685ee248c3fa5a7
nealrangarajan <nealisgood30@gmail.com>
2023-01-30 19:22:14 -0800

    Stuff

--- CSProject/Main.py
+++ CSProject/Main.py
@@ -1,8 +1,8 @@
 import math
 import sys
 
-from Contract import Contract
-from stocks import printValues
+from CSProject.Contract import Contract
+from CSProject.stocks import printValues
 
 #exercise option
 def calculate(Contract, moneyRange):
@@ -16,56 +16,28 @@ def calculate(Contract, moneyRange):
 
 
 #Sell to close
-#you mfs do this
 def calculate(Contract, moneyRange, stc, dayRange):
     rangeVal = int(Contract.getStrike())
     rangeVal2 = int(Contract.getStrike())
-    #for i in range(dayRange):
-        #print("     day: ", i, "     ", end = "")
+    for i in range(dayRange):
+        print("     day: ", i, "     ", end = "")
     for i in range(rangeVal-int(moneyRange/2), rangeVal2+int(moneyRange/2)):
-        for day in range(dayRange):
+        for i in range(dayRange):
+            profit = float(Contract.getPrice())*100 - float()
 
-            print((Contract.getTheta()))
 
-            gammaGrowth = (i-float(Contract.getStrike())) * float(Contract.getGamma())
-            thetaDecay = float(Contract.getTheta())*day
 
-            greekDelta = gammaGrowth + (thetaDecay)
 
-            print(Contract.getGamma())
 
-            #print(greekDelta)
-
-            profit = (float(Contract.getPrice()) + greekDelta)*100
-
-def calculatePut(Contract, moneyRange):
-    rangeVal = int(Contract.getStrike())
-    rangeVal2 = int(Contract.getStrike())
-    for i in range(rangeVal - int(moneyRange / 2), rangeVal2 + int(moneyRange / 2)):
-        print(i)
-        profit = (float(Contract.getStrike())-i) * 100 - float(Contract.getPrice()) * 100
-        print("For market price of: ", i, "your profit is ", profit)
-    print("hello")
-
-def calculatePut(Contract, moneyRange, stc, dayRange):
-    rangeVal = int(Contract.getStrike())
-    rangeVal2 = int(Contract.getStrike())
-    for i in range(rangeVal - int(moneyRange / 2), rangeVal2 + int(moneyRange / 2)):
-        print(i)
-        profit = (float(Contract.getStrike())-i) * 100 - float(Contract.getPrice()) * 100
-        print("For market price of: ", i, "your profit is ", profit)
-    print("hello")
 
 class Main(Contract):
-
-
-    test = Contract(92.5, -0.0032, 0.72, "023-02-10", 0.09)
-
     data = printValues("GOOG")
-    calculate(data[3], 20, True, 20)
-
+    for d in data:
+        print(d)
+    print(data)
     x = 1
     for c in data:
-        if c.getDate().__contains__("2023-02-10"):
+        if c.getDate().__contains__("GOOG"):
             print(x, " - ", c)
             x = x + 1
+    calculate(data[6], 20, True, 7)
--- CSProject/stocks.py
+++ CSProject/stocks.py
@@ -12,15 +12,14 @@ from Contract import Contract
 
 
 def printValues(symbol):
-  scrape_url = 'https://www.optionistics.com/quotes/stock-option-chains/' + symbol + '?symbol=' + symbol + '&chtype=0&nonstd=-1&greeks=1&mn1min=21&mn1max=27&expmin=0&expmax=2&ovmin=0&ovmax=6&strike=&expiry=&op=chains&date=20230127&prevsym=CSCO&clear=0&prevns=-1&prevns=CSCO&v=1'
-
+  #scrape_url = 'https://www.optionistics.com/quotes/stock-option-chains/' + symbol + '?symbol=' + symbol + '&chtype=0&nonstd=-1&greeks=1&mn1min=21&mn1max=27&expmin=0&expmax=2&ovmin=0&ovmax=6&strike=&expiry=&op=chains&date=20230127&prevsym=CSCO&clear=0&prevns=-1&prevns=CSCO&v=1'
+  scrape_url = "https://www.optionistics.com/quotes/stock-option-chains/' + symbol + '?symbol=' + symbol + '&chtype=0&nonstd=-1&greeks=1&mn1min=4&mn1max=10&expmin=0&expmax=2&ovmin=0&ovmax=6&strike=&expiry=&op=chains&date=20230130&prevsym=ASA&clear=0&prevns=-1&prevns=ASA&v=1"
   html_text = requests.get(scrape_url).text
   soup = BeautifulSoup(html_text, 'html.parser')
   tables = soup.find_all('table')
 
   data=[]
   for table in tables:
-
     date=""
     for row in table.find_all('tr'):
       columns = row.find_all('td')
@@ -31,27 +30,19 @@ def printValues(symbol):
           if col.has_attr('class') and col.attrs.get("class")[0] != "disclaimer":
             date=col.text
 
-      if col_length >= 17 and col.has_attr('class') and (col.attrs.get("class")[0] != "inmoney" or col.attrs.get("class")[0] != "outmoney"):
+      if col_length >= 17 and columns[0].has_attr('class'):
+        #print("---------------------")
+        #print(columns[0].attrs)
         strike = columns[0].text
+        #print(strike)
         if (strike != "Strike"):
           price= columns[4].text
           theta = columns[12].text
           gamma = columns[13].text
-          output = "strike:" + strike + ", price:" + price + ", theta:" + theta + ", gamma:" + gamma
-          #print(output)
-
           c = Contract(strike, theta, price, date, gamma)
-
-          output_map = {
-            "date": date,
-            "strike": strike,
-            "price": price,
-            "theta": theta,
-            "gamma": gamma
-          }
           data.append(c)
-    return data
 
+    return data
 
 
 
